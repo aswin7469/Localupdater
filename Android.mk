23074d7088c696f71606df8f63d494d45cfbeca8
Gabriele M <moto.falcon.git@gmail.com>
2020-05-04 21:12:07 -0700

    LocalUpdater: add UpdaterStudio target

--- Android.mk
+++ Android.mk
@@ -28,3 +28,20 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := UpdaterStudio
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_SUFFIX := -timestamp
+updater_system_deps := $(call java-lib-deps,framework)
+updater_system_libs_path := $(abspath $(LOCAL_PATH))/system_libs
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): $(updater_system_deps)
+	$(hide) mkdir -p $(updater_system_libs_path)
+	$(hide) rm -rf $(updater_system_libs_path)/*.jar
+	$(hide) cp $(updater_system_deps) $(updater_system_libs_path)/framework.jar
+	$(hide) echo "Fake: $@"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) touch $@
