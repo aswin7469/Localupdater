2e2fda1f2c2bfdb856f30d3b4c2bd2895c59eae3
Anay Wadhera <anay1018@gmail.com>
2021-02-15 21:12:44 -0800

    LocalUpdater: remove broadcastmanager stuff
    unused bloat
    
    Change-Id: Id0f144880096953dcbd5ef7951acf53eecf4234b

--- src/com/statix/updater/MainViewController.java
+++ src/com/statix/updater/MainViewController.java
@@ -3,8 +3,6 @@ package com.statix.updater;
 import android.content.Context;
 import android.os.Handler;
 
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-
 import com.statix.updater.model.ABUpdate;
 
 import java.util.ArrayList;
@@ -16,7 +14,6 @@ public class MainViewController {
     private final Context mContext;
     private Handler mUiThread;
     private Handler mBgThread = new Handler();
-    private final LocalBroadcastManager mBroadcastManager;
 
     private static MainViewController sInstance = null;
 
@@ -30,7 +27,6 @@ public class MainViewController {
     }
 
     private MainViewController(Context ctx) {
-        mBroadcastManager = LocalBroadcastManager.getInstance(ctx);
         mContext = ctx;
         mUiThread = new Handler(ctx.getMainLooper());
     }
